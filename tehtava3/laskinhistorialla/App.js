d96ad7140b36e916b39a619454705cac88f1e5b3
Simo-Pekka Varis <simo-pekkavaris@computer.local>
2022-09-12 20:11:30 +0300

    update

--- tehtava3/laskinhistorialla/App.js
+++ tehtava3/laskinhistorialla/App.js
@@ -4,18 +4,22 @@ export default function App() {
 
   const [num1, setNum1] = useState(0);
   const [num2, setNum2] = useState(0);
+  const [result, setResult] = useState('');
   const [data, setData] = useState([]);
 
   const plus = () => {
+    setResult(parseInt(num1) + parseInt(num2));
     setData([...data, {key: num1 + ' + ' + num2 + ' = ' + (parseInt(num1) + parseInt(num2))}]);
   }
 
   const minus = () => {
+    setResult(num1 - num2);
     setData([...data, {key: num1 + ' - ' + num2 + ' = ' + (num1 - num2)}]);
   }
   
   return (
     <View style={styles.container}>
+      <Text>Result: {result}</Text>
       <TextInput
       keyboardType='numeric'
       placeholder={'Enter number here'}
